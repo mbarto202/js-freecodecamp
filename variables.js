9f4b70f049dd5db5128aa39a7df8ccd11e8d011e
mbarto202 <michael.d.barto@gmail.com>
2024-08-17 09:58:32 -0400

    initial commit

---
+++ variables.js
@@ -0,0 +1,21 @@
+let message = 'hello'
+
+let numOfDonuts = 12;
+let pi = 3.14;
+
+let veryLargeNum = 54321n;
+
+let loveCoding = true;
+
+let favoriteColor;
+console.log(favoriteColor);
+
+message = null;
+
+const uniqueKey = Symbol();
+
+let course = {
+    name: 'js tutorial',
+    hours: 3
+};
+
