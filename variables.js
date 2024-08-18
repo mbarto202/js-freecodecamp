4fbf58edad315731073cff194735106e6d63a852
mbarto202 <michael.d.barto@gmail.com>
2024-08-18 17:26:19 -0400

    loops

--- variables.js
+++ variables.js
@@ -1,21 +1,72 @@
-let message = 'hello'
+// let message = 'hello'
 
-let numOfDonuts = 12;
-let pi = 3.14;
+// let numOfDonuts = 12;
+// let pi = 3.14;
 
-let veryLargeNum = 54321n;
+// let veryLargeNum = 54321n;
 
-let loveCoding = true;
+// let loveCoding = true;
 
-let favoriteColor;
-console.log(favoriteColor);
+// let favoriteColor;
+// console.log(favoriteColor);
 
-message = null;
+// message = null;
 
-const uniqueKey = Symbol();
+// const uniqueKey = Symbol();
+
+//------------objects-------------
+
+// let course = {
+//     name: 'js tutorial',
+//     hours: 3
+// };
+
+// // console.log(course.name);
+
+// // course.name = 'michael'
+
+// // console.log(course['name']);
+
+// let property = 'hours'
+
+// console.log((course[property]));
+
+//-----------arrays-------------
+
+// let productColors = ['blue', 'red']
+
+// console.log(productColors[0]);
+
+//---------Functions------------
+
+// function sayHi(name) {
+//     console.log('Hi ' + name);
+// }
+
+// sayHi('Michael')
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+
+// console.log(multiply(2, 3));
+
+//-----------Loops-------------
 
 let course = {
-    name: 'js tutorial',
-    hours: 3
-};
+    name: 'michael',
+    age: 25
+}
 
+for(let key in course){
+    console.log(course[key]); 
+}
+
+let nums = [1, 2, 3, 4];
+for(let i = 0; i < nums.length; i++){
+    console.log(nums[i]);
+}
+
+for(let elements of nums){
+    console.log(elements);
+}
