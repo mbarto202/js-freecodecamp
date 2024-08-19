6f9f923e78e3d790ff072d7c4b2d8dc4e53118b3
mbarto202 <michael.d.barto@gmail.com>
2024-08-18 23:07:33 -0400

    codewars

--- variables.js
+++ variables.js
@@ -53,20 +53,57 @@
 
 //-----------Loops-------------
 
-let course = {
-    name: 'michael',
-    age: 25
-}
+// let course = {
+//     name: 'michael',
+//     age: 25
+// }
 
-for(let key in course){
-    console.log(course[key]); 
-}
+// for(let key in course){
+//     console.log(course[key]); 
+// }
 
-let nums = [1, 2, 3, 4];
-for(let i = 0; i < nums.length; i++){
-    console.log(nums[i]);
-}
+// let nums = [1, 2, 3, 4];
+// for(let i = 0; i < nums.length; i++){
+//     console.log(nums[i]);
+// }
 
-for(let elements of nums){
-    console.log(elements);
-}
+// for(let elements of nums){
+//     console.log(elements);
+// }
+
+//-------fizzbuzz----------
+
+// function fizzBuzz(number) {
+//     if (number % 3 === 0 && number % 5 === 0){
+//         return 'fizzbuzz'
+//     }
+//     else if(number % 3 === 0){
+//         return 'fizz'
+//     }
+//     else if(number % 5 === 0){
+//         return 'buzz'
+//     }
+//     else {
+//         return number
+//     }
+// }
+
+// console.log(fizzBuzz(15));
+// console.log(fizzBuzz(10));
+// console.log(fizzBuzz(9));
+// console.log(fizzBuzz(1));
+
+
+//-------evennumbers------------
+
+// function displayEvenNumbers(array){
+//     for(const number of array) {
+//         if(number % 2 === 0 ){
+//             console.log(number);
+//         }
+//     }
+// }
+
+// displayEvenNumbers([1,2,3,4,5])
+
+
